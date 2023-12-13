21840b78cf2d474098b30a38fd41f33851388f24
AzzaFaheema <150208608+AzzaFaheema@users.noreply.github.com>
2023-12-13 11:33:07 +0530

    Shuffle_element_list.py

---
+++ Shuffle_element_list.py
@@ -0,0 +1,11 @@
+import random
+a=["aaa",1,"cccc",2,"ddddd"]
+print("original list:",a)
+random.shuffle(a)
+print("shuffled list:",a)
+
+OUTPUT
+
+original list: ['aaa', 1, 'cccc', 2, 'ddddd']
+shuffled list: [1, 'aaa', 2, 'cccc', 'ddddd']
+​
