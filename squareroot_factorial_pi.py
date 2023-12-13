cc9976c0d1f29936745c6b4f2b87445dc7681541
AzzaFaheema <150208608+AzzaFaheema@users.noreply.github.com>
2023-12-13 10:46:23 +0530

    squareroot_factorial_pi.py

---
+++ squareroot_factorial_pi.py
@@ -0,0 +1,14 @@
+import math
+
+number = int(input("Enter a number: "))
+
+print("Square root of",number,":",math.sqrt(number))
+print("Factorial of",number,":",math.factorial(number))
+print("Value of pi:",math.pi)
+
+OUTPUT
+
+Enter a number: 4
+Square root of 4 : 2.0
+Factorial of 4 : 24
+Value of pi: 3.141592653589793
