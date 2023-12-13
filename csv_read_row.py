df4f97d8b6f59dc12f24f5f8122ab02a0f8363a4
AzzaFaheema <150208608+AzzaFaheema@users.noreply.github.com>
2023-12-13 12:03:01 +0530

    csv_read_row.py

---
+++ csv_read_row.py
@@ -0,0 +1,17 @@
+import csv
+
+def read_csv_file(file_path):
+    with open(file_path, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        
+        for row in csv_reader:
+            print(row)
+
+file_path = 'csv.txt'
+
+read_csv_file(file_path)
+
+OUTPUT
+
+['hey ']
+['hello']
